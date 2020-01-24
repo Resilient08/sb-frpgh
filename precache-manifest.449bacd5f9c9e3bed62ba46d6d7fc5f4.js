ae8d6f68d1662454cebae58be97a6bcbbdc9abcb
Resilient08 <58645288+Resilient08@users.noreply.github.com>
2020-01-24 11:24:43 -0500

    Updates

---
+++ precache-manifest.449bacd5f9c9e3bed62ba46d6d7fc5f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a81a291486f1afa41a2e88809f2a6a",
+    "url": "/sb-frpgh/index.html"
+  },
+  {
+    "revision": "1e5137aa7559b867d4d7",
+    "url": "/sb-frpgh/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a7e1e594a5f1cffc6dcf",
+    "url": "/sb-frpgh/static/css/main.4631185f.chunk.css"
+  },
+  {
+    "revision": "1e5137aa7559b867d4d7",
+    "url": "/sb-frpgh/static/js/2.71897e02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sb-frpgh/static/js/2.71897e02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7e1e594a5f1cffc6dcf",
+    "url": "/sb-frpgh/static/js/main.3cd0d136.chunk.js"
+  },
+  {
+    "revision": "9e28ebb6138a1bddf56a",
+    "url": "/sb-frpgh/static/js/runtime-main.12740df4.js"
+  }
+]);
